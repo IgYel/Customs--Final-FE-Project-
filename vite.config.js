a4c8d4788d86c2d2e04326b95115a021342c094c
IgYel <cutelyfoxy@gmail.com>
2024-09-27 23:33:13 +0200

    fixes for deploy

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Customs--Final-FE-Project-/',  // Замените на имя вашего репозитория
+  build: {
+    outDir: 'dist', // Убедитесь, что сборка идёт в папку dist
+  }
+});
