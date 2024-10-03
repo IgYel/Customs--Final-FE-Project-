ce4bba727311b4dfd6ccb3ee626ff1900a515f03
IgYel <cutelyfoxy@gmail.com>
2024-10-04 01:13:12 +0200

    try 404

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,27 @@
-import '../Styles/style.scss';
-import './themeSwitch';
-import './scroll';
-import './cursor';
-import './popUp';
-import './GuitarsShowSort';
-import './language';
-import './contacts';
-import './registration';
-import './BackEnd';
+import "../Styles/style.scss";
+import "./themeSwitch";
+import "./scroll";
+import "./cursor";
+import "./popUp";
+import "./GuitarsShowSort";
+import "./language";
+import "./contacts";
+import "./registration";
+import "./BackEnd";
+
+(function (l) {
+  if (l.search[1] === "/") {
+    var decoded = l.search
+      .slice(1)
+      .split("&")
+      .map(function (s) {
+        return s.replace(/~and~/g, "&");
+      })
+      .join("?");
+    window.history.replaceState(
+      null,
+      null,
+      l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+})(window.location);
