0a470fe5960dff9d7f866cf12ff8c462c2633075
IgYel <cutelyfoxy@gmail.com>
2024-10-04 01:18:24 +0200

    delete 404.html

--- scripts/main.js
+++ scripts/main.js
@@ -8,20 +8,3 @@ import "./language";
 import "./contacts";
 import "./registration";
 import "./BackEnd";
-
-(function (l) {
-  if (l.search[1] === "/") {
-    var decoded = l.search
-      .slice(1)
-      .split("&")
-      .map(function (s) {
-        return s.replace(/~and~/g, "&");
-      })
-      .join("?");
-    window.history.replaceState(
-      null,
-      null,
-      l.pathname.slice(0, -1) + decoded + l.hash
-    );
-  }
-})(window.location);
